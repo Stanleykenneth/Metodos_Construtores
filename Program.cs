ea6bb70af47233922cd25301ec3bfcd0c60c5141
StanleyKenneth <kstanley_bass@hotmail.com>
2023-12-24 16:15:03 -0300

    [Program] - Utilizando o construtor
    Utilizando os construtores para diminuir a linhas de código.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using ExemploExplorando.Models;
 
-Pessoa p1 = new Pessoa();
-p1.nome = "Fulano";
-p1.sobrenome = "de Tal";
+Pessoa p1 = new Pessoa("Fulano", "de Tal");
+// p1.nome = "Fulano";
+// p1.sobrenome = "de Tal";
 
-Pessoa p2 = new Pessoa();
-p2.nome = "Beltrano";
-p2.sobrenome = "da silva";
+Pessoa p2 = new Pessoa("Beltrano", "da Silva");
+// p2.nome = "Beltrano";
+// p2.sobrenome = "da silva";
 
 
 Curso cursoDeCsharp = new Curso();
